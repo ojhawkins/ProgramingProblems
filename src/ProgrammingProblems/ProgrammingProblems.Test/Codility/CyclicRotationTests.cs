f71a9fd6c91feb9465bb88cce17b4af28c4b781b
Owain Hawkins <ojhawkins@gmail.com>
2019-12-01 10:11:41 +1100

    add cyclic rotation and tests

---
+++ src/ProgrammingProblems/ProgrammingProblems.Test/Codility/CyclicRotationTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using ProgrammingProblems.Codility;
+
+namespace ProgrammingProblems.Test.Codility
+{
+    public class CyclicRotationTests
+    {
+        [TestCaseSource(typeof(CyclicRotationTestCases), nameof(CyclicRotationTestCases.TestCases))]
+        public int[] Fib_frog_solution_returns_the_correct_answer(int[] array, int rotateAmount)
+        {
+            var cyclicRotation = new CyclicRotation();
+            var solution = cyclicRotation.Solution(array, rotateAmount);
+
+            return solution;
+        }
+    }
+
+    public class CyclicRotationTestCases
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new[] { 0, 0 }, 1).Returns(new[] { 0, 0 });
+                yield return new TestCaseData(new[] { 0 }, 5).Returns(new[] { 0 });
+                yield return new TestCaseData(new[] { 0, 0, 0, 0 }, 20).Returns(new[] { 0, 0, 0, 0 });
+                yield return new TestCaseData(new[] { 1, 2, 3, 4 }, 1).Returns(new[] { 4, 1, 2, 3 });
+            }
+        }
+    }
+}
--- src/ProgrammingProblems/ProgrammingProblems/Codility/CyclicRotation.cs
+++ src/ProgrammingProblems/ProgrammingProblems/Codility/CyclicRotation.cs
@@ -1,10 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ProgrammingProblems.Codility
 {
-    class CyclicRotation
+    public class CyclicRotation
     {
+        public int[] Solution(int[] array, int rotateAmount)
+        {
+            if (array.Length == 0)
+            {
+                return array;
+            }
+
+            var shiftAmount = rotateAmount % array.Length;
+            var result = new int [array.Length];
+
+            Array.Copy(array, array.Length - shiftAmount, result, 0, shiftAmount);
+            Array.Copy(array, 0, result, shiftAmount, array.Length - shiftAmount);
+
+            return result;
+        }
     }
 }

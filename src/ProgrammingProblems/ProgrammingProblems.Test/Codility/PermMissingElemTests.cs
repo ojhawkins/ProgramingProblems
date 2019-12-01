7998e14e7fe08f660a18c0d7b76a72f906483c59
Owain Hawkins <ojhawkins@gmail.com>
2019-12-01 14:47:32 +1100

    perm missing solution and tests

---
+++ src/ProgrammingProblems/ProgrammingProblems.Test/Codility/PermMissingElemTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using ProgrammingProblems.Codility;
+
+namespace ProgrammingProblems.Test.Codility
+{
+    public class PermMissingElemTests
+    {
+        [TestCaseSource(typeof(PermMissingElemTestCases), nameof(PermMissingElemTestCases.TestCases))]
+        public int Perm_missing_loop_solution_returns_the_correct_value(int[] numbers)
+        {
+            var permMissingElem = new PermMissingElem();
+            var solution = permMissingElem.SolutionCount(numbers);
+
+            return solution;
+        }
+
+        [TestCaseSource(typeof(PermMissingElemTestCases), nameof(PermMissingElemTestCases.TestCases))]
+        public int Perm_missing_xor_solution_returns_the_correct_value(int[] numbers)
+        {
+            var permMissingElem = new PermMissingElem();
+            var solution = permMissingElem.SolutionXor(numbers);
+
+            return solution;
+        }
+    }
+
+    public class PermMissingElemTestCases
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new[] { 1, 2, 4 }).Returns(3);
+                yield return new TestCaseData(new[] { 1, 2, 4, 3, 6 }).Returns(5);
+            }
+        }
+    }
+}
---
+++ src/ProgrammingProblems/ProgrammingProblems/Codility/PermMissingElem.cs
@@ -0,0 +1,29 @@
+﻿namespace ProgrammingProblems.Codility
+{
+    public class PermMissingElem
+    {
+        public int SolutionCount(int[] arr)
+        {
+            //As they are incremented by 1 from 1
+            //Count the expected result less the actual
+            var result = 1;
+            for (var i = 2; i <= arr.Length; i++)
+            {
+                result += i - arr[i - 2];
+            }
+
+            return result;
+        }
+
+        public int SolutionXor(int[] arr)
+        {
+            var result = 0 ^ 1;
+            for (var i = 2; i <= arr.Length; i++)
+            {
+                result ^= i ^ arr[i - 2];
+            }
+
+            return result;
+        }
+    }
+}
